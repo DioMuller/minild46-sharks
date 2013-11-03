0e5f672885844e075f33f7ed9aee07b8cb006500
Diogo Muller <diogo.muller@live.com>
2013-11-03 08:30:29 -0200

    INT: First version, shark and man swiming.

---
+++ SharkGame/Assets/Scripts/SharkBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharkBehavior : MonoBehaviour
+{
+    public Transform bloodParticle;
+
+    private bool coroutineStarted = false;
+
+    /// <summary>
+    /// Called when the shark collides with other objects.
+    /// </summary>
+    /// <param name="other">Other Object.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if( other.tag == "Player" )
+        {
+            if( bloodParticle != null )
+            {
+                bloodParticle.GetComponent<ParticleSystem>().Play();
+            }
+
+            if( !coroutineStarted )
+            {
+                coroutineStarted = true;
+                StartCoroutine(DestroyPlayer(other));
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (bloodParticle != null)
+        {
+            bloodParticle.GetComponent<ParticleSystem>().Stop();
+        }
+    }
+
+    IEnumerator DestroyPlayer(Collider other)
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(other.gameObject);
+    }
+}
---
+++ SharkGame/Assets/Scripts/SwimingBehavior.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwimingBehavior : MonoBehaviour 
+{
+    #region Inspector Variables
+    /// <summary>
+    /// Water particle effects.
+    /// </summary>
+    public Transform waterEffect;
+
+    /// <summary>
+    /// Current Speed
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// Current Position
+    /// </summary>
+    public float position;
+
+    /// <summary>
+    /// Speed decay.
+    /// </summary>
+    public float decay;
+    #endregion Inspector Variables
+    
+    #region Game Cycle Methods
+    /// <summary>
+    /// Use this for initialization.
+	/// </summary>
+	void Start () 
+    {
+	    position = transform.position.x;
+	}
+	
+	/// <summary>
+    ///  Update is called once per frame.
+	/// </summary>
+	void Update () 
+    {
+	    if( speed > 0f ) 
+        {
+            speed -= (decay * Time.deltaTime);
+            position += (speed * Time.deltaTime);
+        }
+        
+        if( waterEffect != null )
+        {
+            waterEffect.GetComponent<ParticleSystem>().emissionRate = speed * 5f;
+        }
+
+        // TODO: Remove this from here.
+        transform.position = new Vector3( position, transform.position.y, transform.position.z );
+
+	}
+    #endregion Game Cycle Methods
+}
